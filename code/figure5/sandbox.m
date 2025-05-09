e6533d83ab31cd976417d3079d5e868434dba385
Thomas Zhihao Luo <thomas.zhihao.luo@gmail.com>
2025-05-09 17:43:20 -0400

    figure5

--- code/figure5/sandbox.m
+++ code/figure5/sandbox.m
@@ -34,8 +34,9 @@ for cond = ["obsv", "pred"]
         indices = nTc.(cond).recording_id == recording_ids(i);
         has_nTc = ~isnan(nTc.(cond).commitment_timestep(indices));
         G = discretize(absnormdiff(indices), nbins);
-        Y.(cond)(i,:) = splitapply(@mean,has_nTc, G)/mean(has_nTc);
+        Y.(cond)(i,:) = splitapply(@mean,has_nTc, G);
     end
+    Y.(cond) = Y.(cond)/mean(~isnan(nTc.(cond).commitment_timestep));
 end
 %
 close all
@@ -64,3 +65,19 @@ ylabel(sprintf('fraction with \\color[rgb]{%f,%f,%f}nTc', colors.nTc))
 
 [analysispath, scriptname] = fileparts(matlab.desktop.editor.getActiveFilename);
 saveas(gcf, fullfile(analysispath, [scriptname '.svg']))
+%%
+figure('pos', [100 100 200 275])
+common.stylizeaxes
+set(gca, 'fontsize', 18)
+% y = [mean(~isnan(nTc.obsv.commitment_timestep)), mean(~isnan(nTc.pred.commitment_timestep))
+% bar(1,y
+% plot(1, mean(~isnan(nTc.obsv.commitment_timestep)), 'ko', 'markersize', 8, 'linewidth', 1)
+% plot([1.75 2.25], mean(~isnan(nTc.pred.commitment_timestep))*[1,1], '-', 'color', colors.pred, 'linewidth', 1)
+bar(1, mean(~isnan(nTc.obsv.commitment_timestep)), 'facecolor', 'none', 'linewidth', 1)
+bar(2, mean(~isnan(nTc.pred.commitment_timestep)), 'edgecolor', 'none', 'facecolor', colors.pred, 'facealpha', 0.5)
+ylim([0 0.5])
+xlim([0.5 2.5])
+yticks([0 0.5])
+xticks([])
+[analysispath, scriptname] = fileparts(matlab.desktop.editor.getActiveFilename);
+saveas(gcf, fullfile(analysispath, [scriptname '_inset.svg']))
